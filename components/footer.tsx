d1f8687f85c71789bf29b7038cdf3a51c3baacec
Iván Ruiz <ivr.644@gmail.com>
2024-02-15 11:03:50 +0100

    Added github to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,6 +10,7 @@ export const Footer: React.FC = () => {
       <div className="flex justify-center p-5">
         <ul>
           <li className="p-2 text-center transition-all hover:text-teal-600"><Link href={`/`}>Posts</Link></li>
+          <li className="p-2 text-center transition-all hover:text-teal-600"><Link href={`https://www.github.com/Ivanruii/glint-blog`}>Github</Link></li>
           <li className="p-2 text-center transition-all hover:text-teal-600"><Link href={`/`}>Sobre el blog</Link></li>
           <li className="p-2 text-center transition-all hover:text-teal-600"><Link href={`/cookies`}>Politica de Cookies</Link></li>
         </ul>
