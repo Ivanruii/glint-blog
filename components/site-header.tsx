9e53a07825868771d3ab352285127d079df2d0b9
Iván Ruiz <ivr.644@gmail.com>
2024-02-15 10:33:59 +0100

    Delete links in site-header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -19,8 +19,8 @@ export const SiteHeader: React.FC = () => {
                         <span className="font-bold">Glint Blog</span>
                     </Link>
                     <nav className="flex items-center gap-6 text-sm">
-                        <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href={`#`}>Acerca del blog</Link>
-                        <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href={`#`}>Posts</Link>
+                        {/* <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href={`#`}>Acerca del blog</Link>
+                        <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href={`#`}>Posts</Link> */}
                     </nav>
                 </div>
                 <div className="flex items-center">
