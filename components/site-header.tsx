3291905a6cfb79513b89c4e4015e2958c88314b1
Iván Ruiz <ivr.644@gmail.com>
2024-02-15 10:35:37 +0100

    Fixed themes

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -5,16 +5,16 @@ import { GlintLogo } from "./icons/logo";
 import { useTheme } from "next-themes";
 
 export const SiteHeader: React.FC = () => {
-    const { theme } = useTheme();
+    const { theme, setTheme } = useTheme();
     return (
         <header className="m-auto sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex items-center justify-between max-w-[120ch] mx-auto h-14">
                 <div className="flex gap-4">
                     <Link href={`/`} className="flex items-center gap-3" style={{ position: "relative", right: "16px" }}>
-                        {theme == 'light' ?
-                            <GlintLogo className="text-black" fill="black" width={50} />
-                            :
+                        {theme == 'dark' ?
                             <GlintLogo className="text-white" fill="white" width={50} />
+                            :
+                            <GlintLogo className="text-black" fill="black" width={50} />
                         }
                         <span className="font-bold">Glint Blog</span>
                     </Link>
