561442a0f4237715895629c1f98e10227d5b9257
Iván Ruiz <ivr.644@gmail.com>
2024-02-25 18:57:58 +0100

    Added AnimatedLink

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
 import React from "react";
+import { AnimatedLink } from "./";
 
 export const Header: React.FC = () => {
   return (
     <h2 className="mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
-      <Link href="/" className="hover:underline">
+      <AnimatedLink href="/" className="hover:underline">
         Glint Blog
-      </Link>
+      </AnimatedLink>
       .
     </h2>
   );
